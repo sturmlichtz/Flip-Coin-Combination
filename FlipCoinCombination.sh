2b7efa9318a05de7e20302a92bc57ea01a4b2091
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-19 18:54:53 +0530

    added features such as storing doublet combination in Dictionary and displaying its percentage

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -4,7 +4,7 @@ declare -A ResultsDict
 singletResults=(H T)
 coinSide=0
 ResultsDict[${singletResults[$coinSide]}]=0
-read -p "How many times you want to flip a single coin :-" limit
+read -p "How many times you want to flip coins :-" limit
 for((i=o;i<$limit;i++))
 do
 	coinSide=$((RANDOM%2))
@@ -15,5 +15,19 @@ do
 	echo "$side won for :- " ${ResultsDict[$side]}
 	percantage=$(( (${ResultsDict[$side]}*100)/$limit ))
 	echo "$side won for $percantage % in singlet combination"
+done
+
+doubletResults=(HH HT TH TT)
+ResultsDict[${doubletResults[$coinSide]}]=0
+for((i=o;i<$limit;i++))
+do
+	coinSide=$((RANDOM%4))
+	ResultsDict[${doubletResults[$coinSide]}]=$((${ResultsDict[${doubletResults[$coinSide]}]}+1))
+done
+for side in ${doubletResults[@]}
+do
+	echo "$side won for :- " ${ResultsDict[$side]}
+	percantage=$(( (${ResultsDict[$side]}*100)/$limit ))
+	echo "$side won for $percantage % in doublet combination"
 done
 
