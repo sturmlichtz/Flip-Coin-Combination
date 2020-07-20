43fe947569499616924fd3c4f9c4fad902e3792c
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-20 21:26:38 +0530

    Rename Dictionary

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 echo "Flip Coin Combination"
-declare -A ResultsDict
+declare -A SinletResultsDict
 singletResults=(H T)
 coinSide=0
-ResultsDict[${singletResults[$coinSide]}]=0
+SinletResultsDict[${singletResults[$coinSide]}]=0
 read -p "How many times you want to flip a single coin :-" limit
 for((i=o;i<$limit;i++))
 do
 	coinSide=$((RANDOM%2))
-	ResultsDict[${singletResults[$coinSide]}]=$((${ResultsDict[${singletResults[$coinSide]}]}+1))
+	SinletResultsDict[${singletResults[$coinSide]}]=$((${SinletResultsDict[${singletResults[$coinSide]}]}+1))
 done
 for side in ${singletResults[@]}
 do
-	echo "$side won for :- " ${ResultsDict[$side]}
-	percantage=$(( (${ResultsDict[$side]}*100)/$limit ))
+	echo "$side won for :- " ${SinletResultsDict[$side]}
+	percantage=$(( (${SinletResultsDict[$side]}*100)/$limit ))
 	echo "$side won for $percantage % in singlet combination"
 done
 
