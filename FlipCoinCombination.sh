495b2cd95401dc783d8e739c871142863337d1fc
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-20 21:41:38 +0530

    Add 3 Dictionary

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,56 +1,78 @@
 #!/bin/bash 
 echo "Flip Coin Combination"
-declare -A SinletResultsDict
+declare -A SingletResultsDict
 singletResults=(H T)
 coinSide=0
-SinletResultsDict[${singletResults[$coinSide]}]=0
+SingletResultsDict[${singletResults[$coinSide]}]=0
 read -p "How many times you want to flip a single coin :-" limit
 for((i=o;i<$limit;i++))
 do
 	coinSide=$((RANDOM%2))
-	SinletResultsDict[${singletResults[$coinSide]}]=$((${SinletResultsDict[${singletResults[$coinSide]}]}+1))
+	SingletResultsDict[${singletResults[$coinSide]}]=$((${SingletResultsDict[${singletResults[$coinSide]}]}+1))
 done
 for side in ${singletResults[@]}
 do
-	echo "$side won for :- " ${SinletResultsDict[$side]}
-	percantage=$(( (${SinletResultsDict[$side]}*100)/$limit ))
+	echo "$side won for :- " ${SingletResultsDict[$side]}
+	percantage=$(( (${SingletResultsDict[$side]}*100)/$limit ))
 	echo "$side won for $percantage % in singlet combination"
 done
 
+declare -A DoubletResultsDict
 doubletResults=(HH HT TH TT)
-ResultsDict[${doubletResults[$coinSide]}]=0
+DoubletResultsDict[${doubletResults[$coinSide]}]=0
 for((i=o;i<$limit;i++))
 do
 	coinSide=$((RANDOM%4))
-	ResultsDict[${doubletResults[$coinSide]}]=$((${ResultsDict[${doubletResults[$coinSide]}]}+1))
+	DoubletResultsDict[${doubletResults[$coinSide]}]=$((${DoubletResultsDict[${doubletResults[$coinSide]}]}+1))
 done
 for side in ${doubletResults[@]}
 do
-	echo "$side won for :- " ${ResultsDict[$side]}
-	percantage=$(( (${ResultsDict[$side]}*100)/$limit ))
+	echo "$side won for :- " ${DoubletResultsDict[$side]}
+	percantage=$(( (${DoubletResultsDict[$side]}*100)/$limit ))
 	echo "$side won for $percantage % in doublet combination"
 done
 
+declare -A TripletResultsDict
 tripletResults=(HHH HHT HTH THH HTT THT TTH TTT)
-ResultsDict[${tripletResults[$coinSide]}]=0
+TripletResultsDict[${tripletResults[$coinSide]}]=0
 for((i=o;i<$limit;i++))
 do
 	coinSide=$((RANDOM%8))
-	ResultsDict[${tripletResults[$coinSide]}]=$((${ResultsDict[${tripletResults[$coinSide]}]}+1))
+	TripletResultsDict[${tripletResults[$coinSide]}]=$((${TripletResultsDict[${tripletResults[$coinSide]}]}+1))
 done
 for side in ${tripletResults[@]}
 do
-	echo "$side won for :- " ${ResultsDict[$side]}
-	percantage=$(( (${ResultsDict[$side]}*100)/$limit ))
+	echo "$side won for :- " ${TripletResultsDict[$side]}
+	percantage=$(( (${TripletResultsDict[$side]}*100)/$limit ))
 	echo "$side won for $percantage % in triplet combination"
 done
 
 winningCombination=${singletResults[1]}
-for side in ${!ResultsDict[@]}
+for side in ${!SingletResultsDict[@]}
 do
-   if [ ${ResultsDict[$side]} -gt ${ResultsDict[$winningCombination]} ]
+   if [ ${SingletResultsDict[$side]} -gt ${SingletResultsDict[$winningCombination]} ]
    then
       winningCombination=$side
    fi
 done
-echo "Winning Combination is "$winningCombination
+echo "Winning Combination in Singlet is "$winningCombination
+
+winningCombination=${doubletResults[1]}
+for side in ${!DoubletResultsDict[@]}
+do
+   if [ ${DoubletResultsDict[$side]} -gt ${DoubletResultsDict[$winningCombination]} ]
+   then
+      winningCombination=$side
+   fi
+done
+echo "Winning Combination in Doublet is "$winningCombination
+
+winningCombination=${tripletResults[1]}
+for side in ${!TripletResultsDict[@]}
+do
+   if [ ${TripletResultsDict[$side]} -gt ${TripletResultsDict[$winningCombination]} ]
+   then
+      winningCombination=$side
+   fi
+done
+echo "Winning Combination in Triplet is "$winningCombination
